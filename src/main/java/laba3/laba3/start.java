41e9e34a9e65e518b30c683989a87d91fd825598
Дмитрий Владимирович Макаров <Dimamakarov1941@yandex.ru>
2020-09-12 14:51:26 +0300

    make 3 task

--- src/main/java/laba3/laba3/start.java
+++ src/main/java/laba3/laba3/start.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class start {
-
 	public static void main(String[] args) {
 		Shape s1 = new Circle(5.5, "RED", false); // Upcast Circle to Shape
 		System.out.println(s1);
@@ -13,15 +12,22 @@ public class start {
 		System.out.println(s1.getPerimeter());
 		System.out.println(s1.getColor());
 		System.out.println(s1.isFilled());
-		// The method getRadius() is undefined for the type Shape
-
 		// Мы создали новый класс Shape на основе на Circle, у них действуют только
 		// общие методы
 		// для того, чтобы раборотал метод getRadius нам нужно создавать
 		// не Shape s1 а Circle s1
+		//
 		// System.out.println(s1.getRadius());
+		System.out.println("______________________");
 
-		// ape s2 = new Shape();
+		Circle c1 = (Circle) s1;
+		System.out.println(c1);
+		System.out.println(c1.getArea());
+		System.out.println(c1.getPerimeter());
+		System.out.println(c1.getColor());
+		System.out.println(c1.isFilled());
+		System.out.println(c1.getRadius());
+		System.out.println("______________________");
 
 		Shape s3 = new Rectangle(1.0, 2.0, "RED", false); // Upcast
 		System.out.println(s3);
@@ -36,32 +42,37 @@ public class start {
 		System.out.println(r1.getArea());
 		System.out.println(r1.getColor());
 		System.out.println(r1.getLength());
+		System.out.println("______________________");
 
 		Shape s4 = new Square(6.6); // Upcast
 		System.out.println(s4);
 		System.out.println(s4.getArea());
 		System.out.println(s4.getColor());
 		// System.out.println(s4.getSide());
-		// У объекта s4 нет метода getSide
-		//
+		// У объекта s4 нет метода getSide, так как он shape, а не square
+		System.out.println("______________________");
 
 		// Take note that we downcast Shape s4 to Rectangle,
 		// which is a superclass of Square, instead of Square
-		// TODO: make comments (Rectangle) s4;
 		Rectangle r2 = (Rectangle) s4;
 		System.out.println(r2);
 		System.out.println(r2.getArea());
 		System.out.println(r2.getColor());
-		System.out.println(r2.getSide());
-		System.out.println(r2.getLength());
+		// у метода Rectangle нет функции getSide()
+		// System.out.println(r2.getSide());
+		System.out.println("______________________");
 
-		// Downcast Rectangle r2 to Square
+		// System.out.println(r2.getLength());
+		// // Невозможно преобразовать Square в Square
 		// Square sq1 = (Square) r2;
+		//
 		// System.out.println(sq1);
 		// System.out.println(sq1.getArea());
 		// System.out.println(sq1.getColor());
 		// System.out.println(sq1.getSide());
+		// // Метод getLength () не определен для типа Square
 		// System.out.println(sq1.getLength());
+
 	}
 
 }
