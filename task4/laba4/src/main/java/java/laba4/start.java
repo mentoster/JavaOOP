74b535fe6250a1ed7718cc39b09f7e5c0b263195
Zamoshnikov Alexander <70809274+AlexClaw2303@users.noreply.github.com>
2020-09-13 14:11:44 +0300

    Update start.java
    another update

--- task4/laba4/src/main/java/java/laba4/start.java
+++ task4/laba4/src/main/java/java/laba4/start.java
@@ -3,6 +3,6 @@ package java.laba4;
 public class start {
 	public static void main(String[] args) {
 		Movable figure = new MovableRectangle(0,0,2,2,1,1);
-		figure.moveUp();
+		figure.moveUP();
 	}
 }
