36b60e692f7b7c2e289acf96908cdb6f6a7bc7ba
Dmitriy <Dimamakarov1941@yandex.ru>
2020-09-19 11:34:36 +0300

    make new project

---
+++ task5/laba5/src/test/java/laba/laba5/Laba5ApplicationTests.java
@@ -0,0 +1,13 @@
+package laba.laba5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
