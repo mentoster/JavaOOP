6ff05133b184c3efd877a482797f5d1db11d9794
Dmitriy <Dimamakarov1941@yandex.ru>
2020-10-03 12:38:37 +0300

    👌 IMPROVE: add tasl 6

---
+++ task 6/laba6/src/main/java/laba6/laba6/Laba6Application.java
@@ -0,0 +1,13 @@
+package laba6.laba6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba6Application.class, args);
+	}
+
+}
