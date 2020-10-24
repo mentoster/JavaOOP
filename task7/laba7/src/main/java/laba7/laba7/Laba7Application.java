c319b63e1c5ed985a72e7672015516b348785237
Dmitriy <Dimamakarov1941@yandex.ru>
2020-10-24 12:49:56 +0300

    feat(me): :sparkles: new task 7

---
+++ task7/laba7/src/main/java/laba7/laba7/Laba7Application.java
@@ -0,0 +1,12 @@
+package laba7.laba7;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba7Application {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ task7/laba7/src/test/java/laba7/laba7/Laba7ApplicationTests.java
@@ -0,0 +1,13 @@
+package laba7.laba7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
